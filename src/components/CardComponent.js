f854e7c609504c425574aa4f05a40119e60f440c
sowmya1408 <sowmyamannemr@gmail.com>
2020-10-13 19:34:13 +0200

    added project details to readme and removed the defined but never used vars in card component

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function CardComponent({ rooms }) {
-  let image;
   return (
     <div className="room-wrapper">
       {rooms
